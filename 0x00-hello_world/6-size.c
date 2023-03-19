a0b09385fc03ed8684a93255cda6f587815ee13d
--add <bashirhamagam@gmail.com>
2023-03-19 11:55:51 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("The size of an int is: %d byte(s)\n", sizeof(int));
+	printf("The size of a char is: %d byte(s)\n", sizeof(char));
+	printf("The size of a long int is: %d byte(s)\n", sizeof(long int));
+	printf("The size of a long long int is: %d byte(s)\n", sizeof(long long int));
+	printf("The size of a float is: %d byte(s)\n", sizeof(float));
+
+	return(0);
+}
