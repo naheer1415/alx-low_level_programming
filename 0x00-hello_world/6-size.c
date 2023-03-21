398e538922d4426dd4180cc6f3693c47848d2979
--add <bashirhamagam@gmail.com>
2023-03-20 19:08:01 -0700

    0x01. C - Variables, if, else, while

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	printf("The size of an int is: %d byte(s)\n", sizeof(int));
-	printf("The size of a char is: %d byte(s)\n", sizeof(char));
+	printf("The size of an char is: %d byte(s)\n", sizeof(char));
+	printf("The size of a int is: %d byte(s)\n", sizeof(int));
 	printf("The size of a long int is: %d byte(s)\n", sizeof(long int));
 	printf("The size of a long long int is: %d byte(s)\n", sizeof(long long int));
 	printf("The size of a float is: %d byte(s)\n", sizeof(float));
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print alphabet in reverse'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
+
