2c047038ea70aaf412ab045d65a200b3d0c76934
--add <bashirhamagam@gmail.com>
2023-03-22 05:00:57 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,14 @@
  *
  * Return: always 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j + 'a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
