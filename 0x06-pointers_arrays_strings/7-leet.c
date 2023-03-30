a782fecca8f7d9f9a64a9766f1440bdfe3fc24be
--add <bashirhamagam@gmail.com>
2023-03-30 07:17:06 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - Leet encoding
+ * @str: String to be chacked
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+				str[i] = b[j];
+		}
+	}
+	return (str);
+}
